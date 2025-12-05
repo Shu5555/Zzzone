f9574ce0403f4e842f2b31e8ac3737a853f8e9f6
kyo09427 <shoulderhill33@gmail.com>
2025-12-06 03:06:08 +0900

    feat: 不具合修正

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Load environment variables from .env file
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "assets/.env");
 
   // Handle Dropbox web auth callback before the app starts
   if (kIsWeb) {
